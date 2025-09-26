c23167fae6a6ef484c2c95a7dee24ac1c6bb6ae5
Marcelo Fortino <165343667+marceloofortino@users.noreply.github.com>
2025-09-26 17:08:22 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,7 @@
-import { createServer } from "./index";
 import express from "express";
 
-const app = createServer();
-
-app.use(express.static("dist/spa"));
-app.get("*", (req, res) => {
-  res.sendFile(process.cwd() + "/dist/spa/index.html");
-});
-
-const port = Number(process.env.PORT || 8080);
-
-app.listen(port, "0.0.0.0", () => {
-  console.log(`[server] listening on :${port}`);
-});
+export function createServer() {
+  const app = express();
+  app.get("/api/ping", (req, res) => res.send("pong"));
+  return app;
+}
