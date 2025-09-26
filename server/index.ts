900e354b0cde4b97f66fe84ea159d57c45cf3bca
Marcelo Fortino <165343667+marceloofortino@users.noreply.github.com>
2025-09-26 15:48:54 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,13 @@ import express from "express";
 
 export function createServer() {
   const app = express();
+
   app.get("/api/ping", (req, res) => res.send("pong"));
+
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
   return app;
 }
